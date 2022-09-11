ec652e3a955347151979e73ffd09d4a94a28c3aa
Nicole Robbins <nicole.robbins@att.net>
2022-09-10 21:05:39 -0500

    commit tree acbfd08acdf4e13c4739ab735ef04e670098d754

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let greeting;
+let destination;
+let tip;
+let farewell;
+
+function scuberGreetingForFeet(someValue){
+ if (someValue <= 400){
+  greeting = "This one is on me!"
+ }
+ else if (someValue > 2500){
+  greeting = "No can do."
+}
+else if (someValue > 2000){
+    greeting = "I will gladly take your thirty bucks."
+}
+else if (someValue > 400){
+      greeting = "That will be twenty bucks."
+}
+return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  let response = (destination === "NYC") ? "Ok, sounds good." : "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      farewell = "Thank you so much.";
+      break;
+    case 'not as generous':
+      farewell = "Thank you.";
+      break;
+    default:
+      farewell = "Bye.";
+  }
+  return farewell;
 }
